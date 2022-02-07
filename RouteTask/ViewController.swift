2388dcbfd7eebb6ab1cd3cbaed2a4e304b389478
Anton Gribanow <anton.gribanow@icloud.com>
2022-02-07 15:03:09 +0100

    add map, add constraints

--- RouteTask/ViewController.swift
+++ RouteTask/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    
+    let mapView : MKMapView = { // инициализируем карту
+        let mapView = MKMapView()
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        return mapView
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setConstraints()
+        
     }
-
-
 }
 
+extension ViewController {
+    
+    func setConstraints() {
+        
+        view.addSubview(mapView)
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            mapView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+        ])
+    }
+}
+
+
+
+ 

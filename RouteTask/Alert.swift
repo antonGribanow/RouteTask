ebd10bebdb7db10a5487c93765d305e4cdc26473
antonGribanow <anton.gribanow@icloud.com>
2022-02-13 19:50:50 +0100

    Add image buttons

---
+++ RouteTask/Alert.swift
@@ -0,0 +1,25 @@
+//
+//  Alert.swift
+//  RouteTask
+//
+//  Created by Anton Gribanow on 07/02/2022.
+//
+
+//import UIKit
+//
+//extension UIViewController {
+//    
+//    func alertAddAdress(title: String) {
+//        
+//        let alertController = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+//        let alertOK = UIAlertAction(title: "OK", style: .default) { (action) in
+//            print("action")
+//        }
+//        
+//        let alertCancel = UIAlertAction(title: "Cancel", style: .default) { (_) in
+//            <#code#>
+//        }
+//    }
+//    
+//    
+//}
